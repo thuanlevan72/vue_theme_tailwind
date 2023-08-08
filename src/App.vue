92214cf2dd2b713ff5bc906c0b00efb51cbb0b34
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-08-08 18:46:17 +0700

    đâsdasd

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app"><h1 class="text-3xl font-bold underline">Hello world!</h1></div>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style>
